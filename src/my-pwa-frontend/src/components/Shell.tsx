0f4fedcf12f78e539ba3dd6b6436898f64be1843
Ray Kim <raykim@com2us.com>
2025-08-21 17:58:12 +0900

    feat: Shell 컴포넌트에서 하단 내비게이션 제거

--- src/my-pwa-frontend/src/components/Shell.tsx
+++ src/my-pwa-frontend/src/components/Shell.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import Paper from "@mui/material/Paper";
-import HomeIcon from "@mui/icons-material/Home";
-import SettingsIcon from "@mui/icons-material/Settings";
 
 const Shell: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [value, setValue] = React.useState(0);
+  // Bottom navigation removed per request.
 
   return (
     <Box
@@ -40,16 +35,7 @@ const Shell: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         {children}
       </Box>
 
-      <Paper sx={{ position: "sticky", bottom: 0 }} elevation={6}>
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(_, newVal) => setValue(newVal)}
-        >
-          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-          <BottomNavigationAction label="Plans" icon={<SettingsIcon />} />
-        </BottomNavigation>
-      </Paper>
+      {/* Bottom navigation removed */}
     </Box>
   );
 };
