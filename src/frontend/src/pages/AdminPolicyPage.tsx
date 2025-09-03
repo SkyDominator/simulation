7b2730d44faab2aa8b5d553895f618171429c599
Ray Kim <raykim@com2us.com>
2025-09-03 16:31:11 +0900

    fix: 개인정보 처리 방침 제목의 띄어쓰기 수정

--- src/frontend/src/pages/AdminPolicyPage.tsx
+++ src/frontend/src/pages/AdminPolicyPage.tsx
@@ -201,7 +201,7 @@ const AdminPolicyPage: React.FC<AdminPolicyPageProps> = ({ setPage }) => {
           sx={{ mb: 2 }}
         >
           <Typography variant="h6" fontWeight={700}>
-            개인정보처리방침
+            개인 정보 처리 방침
           </Typography>
           <Button variant="text" onClick={() => setPage("main")}>
             돌아가기
