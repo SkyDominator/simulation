2a326ee34d02686021719530aa5b565486cbceb0
Ray Kim <iloveulord86@gmail.com>
2025-10-13 01:29:14 +0900

    feat: SharedArrayBuffer 폴리필 추가 및 worker_threads에서 가져오기

--- src/frontend/scripts/vitest-node-polyfill.cjs
+++ src/frontend/scripts/vitest-node-polyfill.cjs
@@ -7,5 +7,54 @@ if (typeof globalThis.crypto === "undefined") {
 }
 
 if (typeof globalThis.SharedArrayBuffer === "undefined") {
-  globalThis.SharedArrayBuffer = ArrayBuffer;
+  let sharedBufferCtor;
+  try {
+    ({ SharedArrayBuffer: sharedBufferCtor } = require("worker_threads"));
+  } catch (error) {
+    // ignore
+  }
+
+  if (typeof sharedBufferCtor === "function") {
+    globalThis.SharedArrayBuffer = sharedBufferCtor;
+  } else {
+    class SharedArrayBufferPolyfill {
+      constructor(length = 0) {
+        this._buffer = new ArrayBuffer(length);
+      }
+    }
+
+    Object.defineProperty(SharedArrayBufferPolyfill.prototype, "byteLength", {
+      get() {
+        return this._buffer.byteLength;
+      },
+    });
+
+    Object.defineProperty(SharedArrayBufferPolyfill.prototype, "resizable", {
+      get() {
+        return false;
+      },
+    });
+
+    Object.defineProperty(SharedArrayBufferPolyfill.prototype, "growable", {
+      get() {
+        return false;
+      },
+    });
+
+    Object.defineProperty(SharedArrayBufferPolyfill.prototype, "slice", {
+      value(begin, end) {
+        return this._buffer.slice(begin, end);
+      },
+    });
+
+    Object.defineProperty(
+      SharedArrayBufferPolyfill.prototype,
+      Symbol.toStringTag,
+      {
+        value: "SharedArrayBuffer",
+      }
+    );
+
+    globalThis.SharedArrayBuffer = SharedArrayBufferPolyfill;
+  }
 }
